9d0706da99cdc8153c4432ac6e6208343cffa6e5
Willem de Groot <gwillem@gmail.com>
2026-02-24 14:47:04 +0100

    refactor: standardize var merging on deepmerge, drop mergo dependency
    PreRun and Run used different merge libraries (deepmerge vs mergo) for
    the same var-merging purpose. Standardize on deepmerge which handles
    nested maps correctly. Removes dario.cat/mergo dependency.

--- internal/runners/runners_run.go
+++ internal/runners/runners_run.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 	"time"
 
-	"dario.cat/mergo"
 	log "github.com/gwillem/go-simplelog"
 	"github.com/gwillem/whip/internal/model"
 	"github.com/ieee0824/go-deepmerge"
@@ -103,8 +102,7 @@ func PreRun(task *model.Task, playVars model.TaskVars) (tr model.TaskResult) {
 		return tr
 	}
 
-	// todo: isolate this
-	// merge global and task vars
+	// merge play vars with task vars (play vars take precedence)
 	mergedVars, err := deepmerge.Merge(map[string]any(playVars), map[string]any(task.Vars))
 	if err != nil {
 		tr.Status = Failed
@@ -112,8 +110,6 @@ func PreRun(task *model.Task, playVars model.TaskVars) (tr model.TaskResult) {
 		return
 	}
 	task.Vars = mergedVars.(map[string]any)
-
-	// todo: merge vars
 	tr = runner.prerun(task)
 	tr.Task = task
 	return tr
@@ -153,9 +149,12 @@ func Run(task *model.Task, playVars model.TaskVars) (tr model.TaskResult) {
 		}
 	}
 
-	if e := mergo.Merge(&task.Vars, playVars); e != nil {
-		return fail(e.Error())
+	// merge play vars with task vars (task vars take precedence)
+	merged, err := deepmerge.Merge(map[string]any(task.Vars), map[string]any(playVars))
+	if err != nil {
+		return fail(err.Error())
 	}
+	task.Vars = merged.(map[string]any)
 
 	// arg substitution, notably for loop {{item}}
 	for k, v := range task.Args {
